df0e3d82d4ba05bed2616f7ceb5fcb20b62150fe
Luka Saric <rpg.ari7@gmail.com>
2019-06-25 15:45:04 +0200

    Handled `jsend` with error http response :wrench:
    - Added debug script command
    - Added http error response handling with proper tests
    - Polished code

--- src/JSendInterceptor.js
+++ src/JSendInterceptor.js
@@ -1,15 +1,25 @@
 import JSendError from './JSendError';
 
-export function JSendInterceptor(response) {
+export function throwJSendError(response) {
   const { data, ...jsend } = response.data;
   Object.assign(response, { data, jsend });
-  if (jsend.status !== 'error' && jsend.status !== 'fail') return response;
-  throw new JSendError(`Request failed with jsend status: ${jsend.status}`, response);
+  if (jsend.status === 'success') return response;
+  if (jsend.status === 'fail') throw new JSendError('Request failed', response);
+  throw new JSendError(`Request returned an error: ${jsend.message}`, response);
 }
 
 export default function apply(axios) {
   return axios.interceptors.response.use(
-    res => JSendInterceptor(res),
-    err => Promise.reject(err)
-  );
+    res => throwJSendError(res),
+    err => {
+      if (!err.response || !err.response.data) throw err;
+      if (err.response.data.status !== 'success') {
+        return throwJSendError(err.response);
+      }
+      const { data, ...jsend } = err.response.data;
+      Object.assign(err, { data, jsend });
+      throw err;
+    });
 }
+
+export { JSendError };
--- tests/JSendInterceptor.test.js
+++ tests/JSendInterceptor.test.js
@@ -2,7 +2,7 @@
 
 import axios from 'axios';
 import jsend from 'jsend';
-import JSendInterceptor from '..';
+import JSendInterceptor, { JSendError } from '..';
 import nock from 'nock';
 
 const BASE_URL = 'https://example.com';
@@ -41,7 +41,9 @@ describe('JSend axios interceptor', () => {
 
       client.get('/test')
         .catch(err => {
+          expect(err).toBeInstanceOf(JSendError);
           expect(err.jsend.status).toBe('fail');
+          expect(err.message).toBe('Request failed');
           done();
         }, done.fail);
     });
@@ -51,10 +53,72 @@ describe('JSend axios interceptor', () => {
     it('Should return error response with status and message (code and data optional)', done => {
       nock(BASE_URL)
         .get('/test')
-        .reply(200, jsend.error({ message: 'Something went wrong!', code: 404 }));
+        .reply(200, jsend.error({ message: 'Something went wrong!', code: 404, data: false }));
 
       client.get('/test')
         .catch(err => {
+          expect(err).toBeInstanceOf(JSendError);
+          expect(err.jsend.status).toBe('error');
+          expect(err.message).toBe('Request returned an error: Something went wrong!');
+          done();
+        }, done.fail);
+    });
+  });
+
+  describe('Network error', () => {
+    it('Should pass-through network error', done => {
+      nock(BASE_URL)
+        .get('/test')
+        .replyWithError('Network error');
+
+      client.get('/test')
+        .catch(err => {
+          expect(err.isAxiosError).toBe(true);
+          done();
+        }, done.fail);
+    });
+  });
+
+  describe('Response with http error status and JSend status `success`', () => {
+    it('Should throw error with `jsend` status and data', done => {
+      nock(BASE_URL)
+        .get('/test')
+        .reply(400, jsend.success('foo'));
+
+      client.get('/test')
+        .catch(err => {
+          expect(err.isAxiosError).toBe(true);
+          expect(err.jsend.status).toBe('success');
+          expect(err.data).toBe('foo');
+          done();
+        }, done.fail);
+    });
+  });
+
+  describe('Response with http error status and JSend status `fail`', () => {
+    it('Should throw error with `jsend` status and data', done => {
+      nock(BASE_URL)
+        .get('/test')
+        .reply(400, jsend.fail('foo'));
+
+      client.get('/test')
+        .catch(err => {
+          expect(err).toBeInstanceOf(JSendError);
+          expect(err.jsend.status).toBe('fail');
+          done();
+        }, done.fail);
+    });
+  });
+
+  describe('Response with http error status and JSend status `error`', () => {
+    it('Should throw error with `jsend` status and message (code and data optional)', done => {
+      nock(BASE_URL)
+        .get('/test')
+        .reply(400, jsend.error({ message: 'Something went wrong!', code: 400, data: false }));
+
+      client.get('/test')
+        .catch(err => {
+          expect(err).toBeInstanceOf(JSendError);
           expect(err.jsend.status).toBe('error');
           done();
         }, done.fail);
